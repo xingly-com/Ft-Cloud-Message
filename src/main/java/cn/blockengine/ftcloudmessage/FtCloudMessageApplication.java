3b97ecf4cdede665765e37c4d46184d669004514
方糖 <admin@asugar.cn>
2023-08-31 13:52:16 +0800

    init project

---
+++ src/main/java/cn/blockengine/ftcloudmessage/FtCloudMessageApplication.java
@@ -0,0 +1,13 @@
+package cn.blockengine.ftcloudmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtCloudMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtCloudMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/blockengine/ftcloudmessage/FtCloudMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.blockengine.ftcloudmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtCloudMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
