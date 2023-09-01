c9a4a79ba4fb128702b00edf76aca371db5cb006
方糖 <admin@asugar.cn>
2023-09-01 22:37:37 +0800

    init utils

---
+++ src/main/java/cn/blockengine/ftcloudmessage/utils/LocateUtils.java
@@ -0,0 +1,36 @@
+package cn.blockengine.ftcloudmessage.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LocateUtils {
+
+    // 获取真实ip
+    public static String getRealIp (HttpServletRequest request) {
+        if (request == null) {
+            return "";
+        }
+        String ip = request.getHeader("X-Requested-For");
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+}
---
+++ src/main/java/cn/blockengine/ftcloudmessage/utils/MessageUtils.java
@@ -0,0 +1,10 @@
+package cn.blockengine.ftcloudmessage.utils;
+
+public class MessageUtils {
+
+    public static void sendNormalMessage (String phone, String content) {
+        // 待定
+    }
+
+
+}
